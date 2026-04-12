63168ccebf193e4dc72a025f25dc61c96b7896d2
2youdev <2youdev@gmail.com>
2026-04-12 23:51:00 +0200

    fixes

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,16 @@
 import app from '../src/app';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default app;
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', 'https://2ymentanance-dashboard.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return;
+  }
+
+  return app(req, res);
+}
